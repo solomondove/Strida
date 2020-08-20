eb2f8fad7b1bad6f279e3afafa43200e4f34ed04
solomondove <solomondove@gmail.com>
2020-08-20 16:15:39 -0700

    css updates

--- frontend/components/session/splash.jsx
+++ frontend/components/session/splash.jsx
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import {Link} from 'react-router-dom'; 
 
 class SplashPage extends React.Component {
     constructor(props){
@@ -22,9 +23,17 @@ class SplashPage extends React.Component {
                 <h1 className="splash-header">The #1 app for runners and cyclists</h1>
                 <div className="splash-body">
                     <p className="splash-image"></p>
-                    <form onSubmit={this.demoSubmit} className="splash-form">
-                        <button className="session-button" id="splash-demo" type="submit">Log In As A Demo User</button>
-                    </form>
+                    <div className="splash-menu">
+                        <form onSubmit={this.demoSubmit} className="splash-form">
+                            <button className="session-button" id="splash-demo" type="submit">Log In As A Demo User</button>
+                        </form>
+                        <br/> 
+                        <footer>
+                        <h5>By signing up for Strida, you agree to the Terms of Service. We don't have a Privacy Policy.</h5>
+                        <br/> 
+                        <h5>Already a member? <Link to="/login" className="splash-link">Log In</Link> </h5>
+                        </footer>
+                    </div>
                 </div>
             </div>
         )
